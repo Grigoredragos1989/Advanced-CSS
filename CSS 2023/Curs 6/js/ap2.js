e3d2d658915d4692af02cdbabea778d3cf9c4640
[Grigoredragos1989] <[grigoredragos21@gmail.com]>
2023-02-27 20:15:09 +0200

    terminat curs 6 Advanced CSS

---
+++ CSS 2023/Curs 6/js/ap2.js
@@ -0,0 +1,9 @@
+// 1. definim o functie
+let adunare = (a, b) => {
+    return a + b;
+}
+
+// 2. apelam functia
+console.log("suma dintre 24 si 46 este", adunare(24, 46));
+
+// 3. in terminal rulam comanda: node ex1.js
